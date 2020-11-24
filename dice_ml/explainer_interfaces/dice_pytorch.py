c50670fd307351b93b0561dc1b3f674e8b02797e
Skylaemmm <wangsihuiskyla@outlook.com>
2020-11-24 09:15:07 +0800

    Reshape for PyTorch model input

--- dice_ml/explainer_interfaces/dice_pytorch.py
+++ dice_ml/explainer_interfaces/dice_pytorch.py
@@ -43,7 +43,8 @@ class DicePyTorch(ExplainerBase):
         self.optimizer_weights = []  # optimizer, learning_rate
 
         # number of output nodes of ML model
-        temp_input = torch.rand([len(self.data_interface.encoded_feature_names)]).float()
+        feature_num = len(self.data_interface.encoded_feature_names)
+        temp_input = torch.rand([feature_num]).float().reshape(1,feature_num)
         self.num_ouput_nodes = len(self.model.get_output(temp_input).data)
 
     def generate_counterfactuals(self, query_instance, total_CFs, desired_class="opposite", proximity_weight=0.5, diversity_weight=1.0, categorical_penalty=0.1, algorithm="DiverseCF", features_to_vary="all", yloss_type="hinge_loss", diversity_loss_type="dpp_style:inverse_dist", feature_weights="inverse_mad", optimizer="pytorch:adam", learning_rate=0.05, min_iter=500, max_iter=5000, project_iter=0, loss_diff_thres=1e-5, loss_converge_maxiter=1, verbose=False, init_near_query_instance=True, tie_random=False, stopping_threshold=0.5, posthoc_sparsity_param=0.1, posthoc_sparsity_algorithm="linear"):
